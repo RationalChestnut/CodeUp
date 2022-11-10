dcf7f4cd2ba92845a5782ff64db22e7a2c25486c
Yixuan Li <lxd520ANNA@gmail.com>
2022-11-10 17:09:15 -0500

    finish V1

--- src/data/events.js
+++ src/data/events.js
@@ -2,19 +2,32 @@ import event from "../assets/event.jpeg";
 import hook from "../assets/hook.png";
 
 const events = [
-    {
-        id: 1,
-        title: "Introduction to Python using Turtle",
-        description:
-            "In this 3 hour bootcamp, middle schoolers will learn the basics of Python with an interactive Python module called Turtle! There will be other activities throughout the event as we know sitting down for three hours can be nearly impossible!",
-        when: "Sunday, November 6th from 1-4pm",
-        where: "Pyle Middle School",
-        cost: 0,
-        signupLink: "https://forms.gle/vb6Ud5dUJ5Nj1u4d8",
-        startTime: new Date(2022, 10, 6, 13),
-        endTime: new Date(2022, 10, 6, 16),
-        images: [event, event, event, event, hook],
-    },
+  {
+    id: 1,
+    title: "Introduction to Python using Turtle",
+    description:
+      "In this 3 hour bootcamp, middle schoolers will learn the basics of Python with an interactive Python module called Turtle! There will be other activities throughout the event as we know sitting down for three hours can be nearly impossible!",
+    when: "Sunday, November 6th from 1-4pm",
+    where: "Pyle Middle School",
+    cost: 0,
+    signupLink: "https://forms.gle/vb6Ud5dUJ5Nj1u4d8",
+    startTime: new Date(2022, 10, 6, 13),
+    endTime: new Date(2022, 10, 6, 16),
+    images: [event, event, event, event, hook],
+  },
+  {
+    id: 1,
+    title: "Introduction to Python using Turtle",
+    description:
+      "In this 3 hour bootcamp, middle schoolers will learn the basics of Python with an interactive Python module called Turtle! There will be other activities throughout the event as we know sitting down for three hours can be nearly impossible!",
+    when: "Sunday, November 6th from 1-4pm",
+    where: "Pyle Middle School",
+    cost: 0,
+    signupLink: "https://forms.gle/vb6Ud5dUJ5Nj1u4d8",
+    startTime: new Date(2022, 10, 6, 13),
+    endTime: new Date(2023, 10, 6, 16),
+    images: [event, event, event, event, hook],
+  },
 ];
 
 export default events;
--- src/pages/Events/Events.page.js
+++ src/pages/Events/Events.page.js
@@ -1,56 +1,102 @@
 import React from "react";
+import { Carousel } from "react-responsive-carousel";
+import { FaArrowAltCircleRight, FaArrowAltCircleLeft } from "react-icons/fa";
+
 import styles from "./Events.module.css";
 
 import events from "../../data/events";
 import { Event } from "../../components/Event/Event";
+import useWindowDimensions from "../../utils/useWindowDimensions";
 
 export const Events = () => {
-    return (
-        <div className={styles.eventsPageContainer} id="events">
-            <div className={styles.customShapeDividerTop1666447409}>
-                <svg
-                    data-name="Layer 1"
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 1200 120"
-                    preserveAspectRatio="none"
-                >
-                    <path
-                        d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z"
-                        className={styles.shapeFill}
-                    ></path>
-                </svg>
-            </div>
-            <h1 className={styles.eventsPageTitle}>Events</h1>
-            <div className={styles.eventsContainer}>
-                {events.map((event) => {
-                    return (
-                        <Event
-                            key={event.id}
-                            title={event.title}
-                            description={event.description}
-                            when={event.when}
-                            where={event.where}
-                            cost={event.cost}
-                            signupLink={event.signupLink}
-                            past={event.endTime < Date.now()}
-                            images={event.images}
-                        />
-                    );
-                })}
-            </div>
-            <div className={styles.customShapeDividerBottom1666447991}>
-                <svg
-                    data-name="Layer 1"
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 1200 120"
-                    preserveAspectRatio="none"
-                >
-                    <path
-                        d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z"
-                        className={styles.shapeFill}
-                    ></path>
-                </svg>
-            </div>
-        </div>
-    );
+  const { width } = useWindowDimensions();
+
+  const renderEvents = events.sort((a, b) => {
+    if (a.startTime > b.startTime) {
+      return -1;
+    } else {
+      if (a.endTime > b.endTime) {
+        return -1;
+      } else {
+        return 1;
+      }
+    }
+  });
+
+  return (
+    <div className={styles.eventsPageContainer} id="events">
+      <div className={styles.customShapeDividerTop1666447409}>
+        <svg
+          data-name="Layer 1"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 1200 120"
+          preserveAspectRatio="none"
+        >
+          <path
+            d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z"
+            className={styles.shapeFill}
+          ></path>
+        </svg>
+      </div>
+      <h1 className={styles.eventsPageTitle}>Events</h1>
+      <div className={styles.eventsContainer}>
+        <Carousel
+          className={styles.carousel}
+          showThumbs={false}
+          showIndicators={width <= 1100}
+          showStatus={false}
+          emulateTouch
+          renderArrowPrev={(clickHandler) => {
+            return (
+              <FaArrowAltCircleLeft
+                onClick={clickHandler}
+                size={40}
+                className={styles.prevArrow}
+                display={width <= 1100 && "none"}
+              />
+            );
+          }}
+          renderArrowNext={(clickHandler) => {
+            return (
+              <FaArrowAltCircleRight
+                onClick={clickHandler}
+                size={40}
+                className={styles.nextArrow}
+                display={width <= 1100 && "none"}
+              />
+            );
+          }}
+        >
+          {renderEvents.map((event) => {
+            return (
+              <Event
+                key={event.id}
+                title={event.title}
+                description={event.description}
+                when={event.when}
+                where={event.where}
+                cost={event.cost}
+                signupLink={event.signupLink}
+                past={event.endTime < Date.now()}
+                images={event.images}
+              />
+            );
+          })}
+        </Carousel>
+      </div>
+      <div className={styles.customShapeDividerBottom1666447991}>
+        <svg
+          data-name="Layer 1"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 1200 120"
+          preserveAspectRatio="none"
+        >
+          <path
+            d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z"
+            className={styles.shapeFill}
+          ></path>
+        </svg>
+      </div>
+    </div>
+  );
 };
