136df8c881fdbdced5b667b8f9e1a174796b7563
Brian Mason <brian.kw.mason@gmail.com>
2022-11-21 21:33:05 -0500

    Event updates

--- src/data/events.js
+++ src/data/events.js
@@ -6,15 +6,15 @@ import python_event_2 from "../assets/python_event_2.jpg";
 const events = [
   {
     id: 0,
-    title: "Learning App Lab with Code.org",
+    title: "Learn JavaScript with Code.org's App Lab",
     description:
       "In this 3 hour bootcamp, middle schoolers will learn the basics of Python with an interactive Python module called Turtle! There will be other activities throughout the event as we know sitting down for three hours can be nearly impossible!",
-    when: "Sunday, November 6th from 1-4pm",
+    when: "Saturday, December 10th from 1-4pm",
     where: "Pyle Middle School",
     cost: 0,
-    signupLink: "https://forms.gle/vb6Ud5dUJ5Nj1u4d8",
-    startTime: new Date(2022, 11, 6, 13),
-    endTime: new Date(2022, 11, 6, 16),
+    signupLink: "https://forms.gle/bsKTpkpZG8J1FxgT6",
+    startTime: new Date(2022, 11, 11, 13),
+    endTime: new Date(2022, 11, 11, 16),
     images: [event],
   },
   {
